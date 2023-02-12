dfeea5bb45ab4104e155d98387ecea152c146a7a
Denis-Davydov-get <slim2001.88@mail.ru>
2023-02-12 09:30:25 +0300

    add lecziya

---
+++ lecziya/Example012_For/Program.cs
@@ -0,0 +1,18 @@
+﻿//Замена одного символа на другой в функции
+string text = "Сегодня мы углубимся в тему и выделим 4 основных группы методов,"
+                +"а также будем использовать их на практике. "
+                +"И познакомимся с новыми языковыми конструкциями, в частности, циклом.";
+
+string Replase(string text, char oldValue, char newValue)
+{
+    string result = String.Empty; // пустой контейтейнер
+    int lenght = text.Length; //Считаем кол-во знаков в тексте
+    for(int i = 0; i < lenght; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replase(text, '|', ' '); 
+Console.WriteLine(newText);
---
+++ lecziya/Example016_Fibonacci/Program.cs
@@ -0,0 +1,10 @@
+﻿double Factorial(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Factorial(n - 1) + Factorial(n - 2);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+
+}
